564f3b29acdfff2d2c29ed3678f911938cffba90
teckenrod <treyeck2@gmail.com>
2024-01-30 20:50:55 -0600

    added files

---
+++ notes/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from .models import Notes
+from django.http import Http404, HttpResponseRedirect
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from .forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    login_url = "/login"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_detail.html"
+
+
+class NotesCreatView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
